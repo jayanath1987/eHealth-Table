f2492fc841be9bf297d102a9cda7d1636e2c7949
Jayanath Liyanage <jayanath1987@gmail.com>
2019-10-24 17:07:57 +0530

    Add files via upload

---
+++ table/views/table_render.php
@@ -0,0 +1,52 @@
+<?php
+/*
+--------------------------------------------------------------------------------
+HHIMS - Hospital Health Information Management System
+Copyright (c) 2011 Information and Communication Technology Agency of Sri Lanka
+<http: www.hhims.org/>
+----------------------------------------------------------------------------------
+This program is free software: you can redistribute it and/or modify it under the
+terms of the GNU Affero General Public License as published by the Free Software 
+Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along 
+with this program. If not, see <http://www.gnu.org/licenses/> 
+
+
+
+
+---------------------------------------------------------------------------------- 
+Date : June 2016
+Author: Mr. Jayanath Liyanage   jayanathl@icta.lk
+
+Programme Manager: Shriyananda Rathnayake
+URL: http://www.govforge.icta.lk/gf/project/hhims/
+----------------------------------------------------------------------------------
+*/
+echo "<div id='prefCont1'></div>";
+$qry = "SELECT ";
+for ($i = 0; $i < count($header); ++$i) {
+    $qry .= $header[$i] . ",";
+}
+$qry = substr($qry, 0, strlen($qry) - 1);
+$qry .= " FROM " . $table . " ";
+
+
+echo "<div id='prefCont1'></div>";
+
+
+
+
+
+$this->load->model('mpager');
+$this->mpager->setSql($qry);
+
+$this->mpager->setDivId("prefCont1");
+$this->mpager->render();
+
+?>
+
